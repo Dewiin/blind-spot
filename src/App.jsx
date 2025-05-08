299f5c27e869ff76306eab443ac897025553a28d
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-05-07 21:32:34 -0400

    Moved the logo

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
     <>
       <div className="wholePage">
         <div className="infoBar"> 
-          <img src="./assets/Logo.png" alt="Logo goes here" width= "50%" />
+          <img src="./Logo.png" alt="Logo goes here" width= "50%" />
         </div>
         <div className="mainBody">
-          <img src="./assets/describe.png" alt="Logo goes here" width = "50%" />
+          <img src="./describe.png" alt="describe goes here" width = "50%" />
           <CameraFeed />
         </div>
       </div>
